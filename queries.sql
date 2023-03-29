07b735bdd5e0012d9f95549070d1c1577cefa800
Uthmanbello <uthmanbello88@gmail.com>
2023-03-29 16:24:57 +0100

    Add queries to remove negative weights

--- queries.sql
+++ queries.sql
@@ -8,3 +8,14 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Update queries to remove negative weights. */
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_sp;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT my_sp;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * from animals;
